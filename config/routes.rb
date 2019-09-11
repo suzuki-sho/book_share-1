842ff939fa264f2ce39d01b25d656554d57e55d7
risottoupdate <freepc.yuki@gmail.com>
2019-09-12 01:46:49 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
 
-  get "/" => "home#top"
+  get "/" => "posts#index"
 
 
 end
