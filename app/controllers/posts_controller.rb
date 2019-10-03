3d10500ce57876362aeab8c3f53e70e50048e6bf
risottoupdate <freepc.yuki@gmail.com>
2019-10-03 10:28:48 +0900

    original

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user
+  before_action :authenticate_user,except: [:index]
   before_action :ensure_correct_user,{only: [:edit, :update, :destroy]}
 
   def index
